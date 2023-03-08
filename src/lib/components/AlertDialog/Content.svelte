ccffc339cdcd1c1d1bc0337610f8207ba2cf404b
TheComputerM <mudit.somani00@gmail.com>
2023-03-08 18:09:23 +0530

    add AlertDialog

---
+++ src/lib/components/AlertDialog/Content.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { Writable } from 'svelte/store';
+
+	import type { DialogStoreType } from '../Dialog/types';
+
+	import { hasContext, getContext } from 'svelte';
+
+	import { CONTEXT } from '../../constants';
+
+	import Dismissable from '../Dismissable';
+	import { createDismissableContext } from '../Dismissable/context';
+
+	if (!hasContext(CONTEXT.DIALOG)) {
+		throw new Error('Dialog.Content must be used inside Dialog.Container');
+	}
+
+	const dialogStore = getContext<Writable<DialogStoreType>>(CONTEXT.DIALOG);
+	const dismissableStore = createDismissableContext();
+
+    function escape(event: CustomEvent<KeyboardEvent>) {
+		dialogStore.update((state) => ({
+			...state,
+			open: false
+		}));
+	}
+</script>
+
+{#if $dialogStore.open}
+	<div
+		role="alertdialog"
+        on:escape={escape}
+		id={$dialogStore.contentID}
+		{...$$restProps}
+		aria-describedby={$dialogStore.descriptionID}
+		aria-labelledby={$dialogStore.titleID}
+		use:Dismissable.Layer={{
+			store: dismissableStore,
+			disableOutsidePointerEvents: true
+		}}
+	>
+		<slot />
+	</div>
+{/if}
---
+++ src/lib/components/AlertDialog/index.ts
@@ -0,0 +1,9 @@
+import Container from '../Dialog/Container.svelte';
+import Close from '../Dialog/Close.svelte';
+import Content from './Content.svelte';
+import Description from '../Dialog/Description.svelte';
+import Overlay from '../Dialog/Overlay.svelte';
+import Title from '../Dialog/Title.svelte';
+import Trigger from '../Dialog/Trigger.svelte';
+
+export default { Container, Close, Content, Description, Overlay, Title, Trigger };
