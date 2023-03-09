dadcf74d1ea55f2e8556c9dc167fb4ef0f928368
TheComputerM <mudit.somani00@gmail.com>
2023-03-09 05:46:05 +0000

    add more tests

--- tests/Accordion.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/teil-ui/docs/components/accordion');
-	await expect(page.getByRole('heading', { name: 'Accordion' })).toBeVisible();
-});
